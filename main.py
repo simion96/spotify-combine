8cf7d6777d596ebc42e830d632383614ed356ac5
Simion Putina <simionp96@gmail.com>
2016-06-27 13:14:16 +0100

    cleaned up

--- main.py
+++ main.py
@@ -11,11 +11,8 @@ import json
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
-    #playlist_id = 'spotify:user:1118158951:playlist:40YEYqJ3SOytr5ll3FNXKd'
     playlist_id = '40YEYqJ3SOytr5ll3FNXKd'
-    track_ids = ['spotify:track:1xalXygnuN9pA9NejSHfJV', '0PO7fVyPLMShxeh9OKjbWB']
-    #track_ids = ['1xalXygnuN9pA9NejSHfJV', '0PO7fVyPLMShxeh9OKjbWB']
-    #playlist_id = sys.argv[2]
+    #track_ids = ['spotify:track:1xalXygnuN9pA9NejSHfJV', '0PO7fVyPLMShxeh9OKjbWB']
     #track_ids = sys.argv[3:]
 else:
     print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
@@ -41,21 +38,14 @@ if token:
 
     mainPlaylist = sp.user_playlist(username, playlist_id, fields='tracks, next')
     items = mainPlaylist['tracks']['items']
-    print json.dumps(items, indent=2)
+    #print json.dumps(items, indent=2)
 
     for i in items:
         #print json.dumps(i['track']['uri'], indent=2)
         targetCurrSongs.append(i['track']['uri'])
-    print "content of list is: "
-    print targetCurrSongs[:]
+    #print "content of list is: "
+    #print targetCurrSongs[:]
 
-    print targetCurrSongs[1]
-    #track1 = sys.argv[2]
-    #print "track to request: " + track1
-
-    #https://open.spotify.com/track/1xalXygnuN9pA9NejSHfJV
-
-    print "track ids[0] is " + track_ids[0]
     results = sp.user_playlist_remove_all_occurrences_of_tracks(user = username, playlist_id = playlist_id, tracks = targetCurrSongs)
 else:
     print("Can't get token for", username)
