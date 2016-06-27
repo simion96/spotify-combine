93c438c2f8720facac2736bb5f8f79f2954a6670
Simion Putina <simionp96@gmail.com>
2016-06-27 13:06:12 +0100

    can now delete shit

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ import json
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
+    #playlist_id = 'spotify:user:1118158951:playlist:40YEYqJ3SOytr5ll3FNXKd'
     playlist_id = '40YEYqJ3SOytr5ll3FNXKd'
-    track_ids = ['5QCTcIMAfMuTGDvttwDBv1', '0PO7fVyPLMShxeh9OKjbWB']
+    track_ids = ['spotify:track:1xalXygnuN9pA9NejSHfJV', '0PO7fVyPLMShxeh9OKjbWB']
+    #track_ids = ['1xalXygnuN9pA9NejSHfJV', '0PO7fVyPLMShxeh9OKjbWB']
     #playlist_id = sys.argv[2]
     #track_ids = sys.argv[3:]
 else:
@@ -48,9 +50,13 @@ if token:
     print targetCurrSongs[:]
 
     print targetCurrSongs[1]
-    track1 = sys.argv[2]
-    print "track to request: " + track1
-    results = sp.user_playlist_remove_all_occurrences_of_tracks(username, playlist_id, track1)
+    #track1 = sys.argv[2]
+    #print "track to request: " + track1
+
+    #https://open.spotify.com/track/1xalXygnuN9pA9NejSHfJV
+
+    print "track ids[0] is " + track_ids[0]
+    results = sp.user_playlist_remove_all_occurrences_of_tracks(user = username, playlist_id = playlist_id, tracks = targetCurrSongs)
 else:
     print("Can't get token for", username)
 
