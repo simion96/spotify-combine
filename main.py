86334152c4c249c414aa1ad50f3728aaef7e4342
Simion Putina <simionp96@gmail.com>
2016-06-28 14:13:44 +0100

    can now combine playlists

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import json
 if len(sys.argv) > 1:
     username = sys.argv[1]
     playlist_id = '40YEYqJ3SOytr5ll3FNXKd'
+    playlists = ['2KYqkzbYBsWTqibOMYl8Pd', '5kfJZEHCsoFwzNHnu7aLsO']
     #track_ids = ['spotify:track:1xalXygnuN9pA9NejSHfJV', '0PO7fVyPLMShxeh9OKjbWB']
     #track_ids = sys.argv[3:]
 else:
@@ -32,6 +33,7 @@ token = util.prompt_for_user_token(username, scope, client_id = clientID, client
 # #doshit
 # #username - 1118158951
 targetCurrSongs = []
+sourceSongs = []
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
@@ -44,9 +46,26 @@ if token:
         targetCurrSongs.append(i['track']['uri'])
 
     sp.user_playlist_remove_all_occurrences_of_tracks(user = username, playlist_id = playlist_id, tracks = targetCurrSongs)
+
+    counter = 0
+    for playlist in playlists:
+        playlist = sp.user_playlist(username, playlists[counter], fields='tracks, next')
+        playlistItems = playlist['tracks']['items']
+        for x in playlistItems:
+            sourceSongs.append(x['track']['uri'])
+        counter+=1
+    print 'source uris: '
+    print sourceSongs
+
+    #sp.user_playlist_add_tracks(username, playlist_id, targetCurrSongs)
+    sp.user_playlist_add_tracks(username, playlist_id, sourceSongs)
+
 else:
     print("Can't get token for", username)
 
 
+
+#spotify:track:1MsyEbEQca1sfC9JnkKnOm
+#spotify:track:2ktzBAD5iGVfK1SSE2gFMk
 
 
